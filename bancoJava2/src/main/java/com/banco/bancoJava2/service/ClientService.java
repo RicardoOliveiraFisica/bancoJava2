ec6dc4f0b17372e61ec6fdb57b8e121b093e9e92
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-09 23:47:23 -0300

    service

--- bancoJava2/src/main/java/com/banco/bancoJava2/service/ClientService.java
+++ bancoJava2/src/main/java/com/banco/bancoJava2/service/ClientService.java
@@ -1,5 +1,28 @@
 package com.banco.bancoJava2.service;
 
-public class ClientService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.banco.bancoJava2.entity.Client;
+import com.banco.bancoJava2.repository.ClientRepository;
+
+@Service
+public class ClientService {
+	private ClientRepository clientRepository;
+	
+	@Autowired
+	public ClientService(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
+
+	public Client registerClient(Client client) {
+		return this.clientRepository.save(client);
+	}
+	
+	public List<Client> returnAllClients() {
+		List<Client> clients = this.clientRepository.findAll();		
+		return clients;
+	}
 }
