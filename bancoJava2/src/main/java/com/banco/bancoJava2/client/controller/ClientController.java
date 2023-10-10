1fa3d40b2947f69ff95e62977cf2c31f15ef1f90
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-09 23:47:39 -0300

    controller

--- bancoJava2/src/main/java/com/banco/bancoJava2/client/controller/ClientController.java
+++ bancoJava2/src/main/java/com/banco/bancoJava2/client/controller/ClientController.java
@@ -1,12 +1,41 @@
 package com.banco.bancoJava2.client.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.bancoJava2.entity.Client;
+import com.banco.bancoJava2.service.ClientService;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("api/v1/bancoricardo")
+@RequestMapping("api/v1/bancoricardo/clientes")
 public class ClientController {
+	private ClientService clientService;
+	
+	@Autowired
+	public ClientController(ClientService clientService) {
+		this.clientService = clientService;
+	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client registerClient(@Valid @RequestBody Client client) {
+		return this.clientService.registerClient(client);
+	}
+	
+	@GetMapping
+	public List<Client> returnAllClients() {
+		return this.clientService.returnAllClients();
+	}
 }
