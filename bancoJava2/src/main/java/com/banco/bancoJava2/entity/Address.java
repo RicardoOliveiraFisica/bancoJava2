c497e3def3d1b6f36a4031c63f745029a389aa04
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-09 23:45:45 -0300

    construcao dos objetos cliente e endereco

---
+++ bancoJava2/src/main/java/com/banco/bancoJava2/entity/Address.java
@@ -0,0 +1,51 @@
+package com.banco.bancoJava2.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String rua;
+	private Integer numero;
+	private String cep;
+	
+	public Address() {}	
+	
+	public Address(String rua, Integer numero, String cep) {
+		super();
+		this.rua = rua;
+		this.numero = numero;
+		this.cep = cep;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}	
+}
--- bancoJava2/src/main/java/com/banco/bancoJava2/entity/Client.java
+++ bancoJava2/src/main/java/com/banco/bancoJava2/entity/Client.java
@@ -1,5 +1,76 @@
 package com.banco.bancoJava2.entity;
 
-public class Client {
+import java.math.BigDecimal;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Client {
+	
+	@Id
+	private String cpf;
+	private String nome;
+	private String telefone;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "endereco_id")
+	private Address endereco;
+	
+	private BigDecimal rendimentoMensal;
+	
+	public Client() {}
+		
+	public Client(String nome, String cpf, String telefone, Address endereco, BigDecimal rendimentoMensal) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.rendimentoMensal = rendimentoMensal;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public Address getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Address endereco) {
+		this.endereco = endereco;
+	}
+	
+	public BigDecimal getRendimentoMensal() {
+		return rendimentoMensal;
+	}
+	
+	public void setRendimentoMensal(BigDecimal rendimentoMensal) {
+		this.rendimentoMensal = rendimentoMensal;
+	}
+	
 }
