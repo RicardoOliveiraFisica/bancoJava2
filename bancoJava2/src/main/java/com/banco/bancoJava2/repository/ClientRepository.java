50a1ae087d47b537d35d2e9dd43f35024440b39f
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-09 23:46:48 -0300

    repository

--- bancoJava2/src/main/java/com/banco/bancoJava2/repository/ClientRepository.java
+++ bancoJava2/src/main/java/com/banco/bancoJava2/repository/ClientRepository.java
@@ -1,5 +1,8 @@
 package com.banco.bancoJava2.repository;
 
-public class ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.banco.bancoJava2.entity.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
