ab00eb70267cb44cdf9716b8c8059c0fbe073d3e
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-11 00:45:47 -0300

    controller recebendo/entregando clientDTO

--- bancoJava2/src/main/java/com/banco/bancoJava2/client/controller/ClientController.java
+++ bancoJava2/src/main/java/com/banco/bancoJava2/client/controller/ClientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banco.bancoJava2.dto.ClientDTO;
 import com.banco.bancoJava2.entity.Client;
 import com.banco.bancoJava2.service.ClientService;
 
@@ -30,8 +31,11 @@ public class ClientController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Client registerClient(@Valid @RequestBody Client client) {
-		return this.clientService.registerClient(client);
+	public ClientDTO registerClient(@Valid @RequestBody ClientDTO clientDTO) {
+		Client client = ClientDTO.returnClient(clientDTO);
+		Client clientSaved = this.clientService.registerClient(client);
+		ClientDTO clientDTOsaved = ClientDTO.returnClientDTO(clientSaved);
+		return clientDTOsaved;
 	}
 	
 	@GetMapping
